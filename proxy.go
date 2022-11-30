bb132b4ae3dd9bf0cf1a8058130d12cdc5c510e0
Ivan Echevarria <ivan@echevarria.io>
2022-11-30 09:33:07 -0700

    forward requests

--- proxy.go
+++ proxy.go
@@ -2,61 +2,89 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
+func panicAndCleanUp(socks []int, err error) {
+	cleanUp(socks)
+	panic(err)
+}
+
+func cleanUp(socks []int) {
+	for _, s := range socks {
+		fmt.Printf("Closing socket fd %v\n", s)
+		err := syscall.Close(s)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
 func main() {
 	sock, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
 	if err != nil {
-		panic(err)
+		panicAndCleanUp([]int{sock}, err)
 	}
 	err = syscall.Bind(sock, &syscall.SockaddrInet4{Port: 8080})
 	if err != nil {
-		panic(err)
+		panicAndCleanUp([]int{sock}, err)
 	}
 
-	// close socket on interrupt
+	// Close socket on interrupt
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	go func() {
 		<-c
-		fmt.Println("\nClosing socket")
-		syscall.Close(sock)
+		cleanUp([]int{sock})
 		os.Exit(0)
 	}()
 
-	err = syscall.Listen(sock, 5)
+	err = syscall.Listen(sock, 1)
 	if err != nil {
-		panic(err)
+		panicAndCleanUp([]int{sock}, err)
 	}
-	nfd, destAddr, err := syscall.Accept(sock)
+	nfd, _, err := syscall.Accept(sock)
 	if err != nil {
-		panic(err)
+		panicAndCleanUp([]int{sock}, err)
 	}
 
 	buf := make([]byte, 2048)
-	n := 1
-	for n != 0 {
-		n, _, err = syscall.Recvfrom(nfd, buf, 0)
-		if err != nil {
-			panic(err)
-		}
-
-		ips, err := net.LookupIP(string(buf[:n-1]))
-		// for _, ip := range ips {
-		// 	if
-		// }
-		fmt.Println(err)
-		fmt.Println(ips)
-
-		fmt.Println(destAddr)
-
-		err = syscall.Sendto(nfd, buf[:n], 0, destAddr)
-		if err != nil {
-			panic(err)
-		}
+	n, _, err := syscall.Recvfrom(nfd, buf, 0)
+	if err != nil {
+		panicAndCleanUp([]int{sock}, err)
 	}
+	fmt.Println(string(buf[:n]))
+
+	// Make a new socket, forward request
+	sock2, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+	if err != nil {
+		panicAndCleanUp([]int{sock, sock2}, err)
+	}
+	err = syscall.Bind(sock2, &syscall.SockaddrInet4{Port: 8088})
+	if err != nil {
+		panicAndCleanUp([]int{sock, sock2}, err)
+	}
+	err = syscall.Connect(sock2, &syscall.SockaddrInet4{Port: 9000, Addr: [4]byte{127, 0, 0, 1}})
+	if err != nil {
+		panicAndCleanUp([]int{sock, sock2}, err)
+	}
+	_, err = syscall.Write(sock2, buf[:n])
+	if err != nil {
+		panicAndCleanUp([]int{sock, sock2}, err)
+	}
+	recvN, _, err := syscall.Recvfrom(sock2, buf, 0)
+	if err != nil {
+		panicAndCleanUp([]int{sock, sock2}, err)
+	}
+	cleanUp([]int{sock2})
+	fmt.Println(string(buf[:recvN]))
+
+	_, err = syscall.Write(nfd, buf[:recvN])
+	if err != nil {
+		panicAndCleanUp([]int{sock}, err)
+	}
+
+	cleanUp([]int{sock})
 }

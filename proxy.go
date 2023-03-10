3a24e9bf53e1a908fb0bf03b71f7423d3264204b
Ivan Echevarria <ivan@echevarria.io>
2023-03-09 22:26:23 -0800

    proper proxying

--- proxy.go
+++ proxy.go
@@ -6,45 +6,80 @@ import (
 )
 
 func main() {
-	fmt.Println("HELLO WORLD")
+	fmt.Println("proxy starting")
 
+	// list on localhost 8080
 	sock, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
 	if err != nil {
 		panic(err)
 	}
-
 	err = syscall.Bind(sock, &syscall.SockaddrInet4{Port: 8080, Addr: [4]byte{127, 0, 0, 1}})
 	if err != nil {
 		panic(err)
 	}
-
 	err = syscall.Listen(sock, 1)
 	if err != nil {
 		panic(err)
 	}
-
-	nfd, sa, err := syscall.Accept(sock)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(nfd)
-	fmt.Println(sa)
-
 	defer syscall.Close(sock)
 
 	for {
+		// accept conn + read incoming req
+		nfd, _, err := syscall.Accept(sock)
+		if err != nil {
+			panic(err)
+		}
 		buf := make([]byte, 4096)
 		n, err := syscall.Read(nfd, buf)
 		if err != nil {
 			panic(err)
 		}
+		fmt.Println("-> got client request")
 
-		n, err = syscall.Write(nfd, buf[:n])
+		// proxy to server
+		var sock2 int
+		success := false
+		// retry proxy conn on EINTR
+		for success != true {
+			sock2, err = syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+			if err != nil {
+				panic(err)
+			}
+
+			err = syscall.Connect(sock2, &syscall.SockaddrInet4{Port: 9000, Addr: [4]byte{127, 0, 0, 1}})
+			if err == syscall.EINTR {
+				continue
+			}
+			if err != nil {
+				panic(err)
+			}
+			success = true
+		}
+		_, err = syscall.Write(sock2, buf[:n])
 		if err != nil {
 			panic(err)
 		}
+		fmt.Println("fwd to server ->")
 
-		fmt.Println(buf[:n])
-		fmt.Println(sock)
+		// read the whole response
+		var res string
+		n = 1
+		for n != 0 {
+			n, _, err = syscall.Recvfrom(sock2, buf, 0)
+			if err != nil {
+				panic(err)
+			}
+			res += string(buf[:n])
+		}
+		syscall.Close(sock2)
+		fmt.Println("read server response <-")
+
+		// forward response back
+		_, err = syscall.Write(nfd, []byte(res))
+		if err != nil {
+			panic(err)
+		}
+		syscall.Close(nfd)
+		fmt.Println("<- response sent to client")
 	}
 }

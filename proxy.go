8116016c8398b80f78bacc3400b981a52dc202a9
Ivan Echevarria <ivan@echevarria.io>
2023-03-13 22:06:46 -0700

    make request struct

--- proxy.go
+++ proxy.go
@@ -1,9 +1,46 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"strings"
 	"syscall"
 )
+
+type Request struct {
+	Method  string
+	Target  string
+	Version string
+	Headers map[string]string
+}
+
+func ReadRequest(request []byte) (*Request, error) {
+	scanner := bufio.NewScanner(bytes.NewReader(request))
+
+	// Read start-line
+	scanner.Scan()
+	startLine := strings.Split(scanner.Text(), " ")
+	if len(startLine) != 3 {
+		return nil, fmt.Errorf("Wrong start line HTTP: %s", startLine)
+	}
+	var r Request
+	r.Method = startLine[0]
+	r.Target = startLine[1]
+	r.Version = startLine[2]
+
+	// Read headers
+	r.Headers = make(map[string]string)
+	for scanner.Scan() {
+		headerLine := strings.Split(scanner.Text(), ": ")
+		if len(headerLine) != 2 {
+			continue
+		}
+		r.Headers[headerLine[0]] = headerLine[1]
+	}
+
+	return &r, nil
+}
 
 func main() {
 	fmt.Println("proxy starting")
@@ -35,6 +72,11 @@ func main() {
 			panic(err)
 		}
 		fmt.Println("-> got client request")
+		r, err := ReadRequest(buf[:n])
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%+v\n", r)
 
 		// proxy to server
 		var sock2 int
@@ -48,6 +90,7 @@ func main() {
 
 			err = syscall.Connect(sock2, &syscall.SockaddrInet4{Port: 9000, Addr: [4]byte{127, 0, 0, 1}})
 			if err == syscall.EINTR {
+				syscall.Close(sock2)
 				continue
 			}
 			if err != nil {

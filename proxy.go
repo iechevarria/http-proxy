c84a4b9850686d7b1f4ecfe54311bc4af4cd4004
Ivan Echevarria <ivan@echevarria.io>
2023-03-20 21:39:26 -0700

    use worker goroutines

--- proxy.go
+++ proxy.go
@@ -125,6 +125,7 @@ func NewConnectedSocket(port int, addr [4]byte) (int, error) {
 		break
 	}
 
+	syscall.SetsockoptInt(sock, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
 	return sock, nil
 }
 
@@ -132,7 +133,7 @@ func ProxyRequest(sock int, request []byte) ([]byte, error) {
 	// send client request
 	_, err := syscall.Write(sock, request)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("ProxyRequest error writing to sock: %s", err)
 	}
 
 	// read the whole server response
@@ -142,95 +143,116 @@ func ProxyRequest(sock int, request []byte) ([]byte, error) {
 	for n != 0 {
 		n, _, err = syscall.Recvfrom(sock, buf, 0)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("ProxyRequest error reading from sock: %s", err)
 		}
 		response = append(response, buf[:n]...)
 	}
-	syscall.Close(sock)
 
 	return response, nil
 }
 
-func main() {
-	fmt.Println("proxy starting")
-	prefix := "/chumba/"
-	// TODO: make this a response obj + marshal
-	cache := make(map[string][]byte)
+func HandleRequest(sock int, backendSock int, cache map[string][]byte) {
+	// TODO: remove
+	prefix := "chumba"
 
+	buf := make([]byte, 2048)
+
+	n, err := syscall.Read(sock, buf)
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := ReadRequest(buf[:n])
+	if err != nil {
+		panic(err)
+	}
+
+	// proxy request along or get cached result
+	var res []byte
+	var ok bool
+	if strings.HasPrefix(req.Target, prefix) {
+		res, ok = cache[req.Target]
+		if ok {
+			fmt.Printf("Returning cached result for %s\n", req.Target)
+		} else {
+			res, err = ProxyRequest(backendSock, buf[:n])
+			if err != nil {
+				panic(err)
+			}
+			cache[req.Target] = res
+			fmt.Printf("Cached result for %s\n", req.Target)
+		}
+	} else {
+		res, err = ProxyRequest(backendSock, buf[:n])
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// resStruct, err := ReadResponse(res)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// forward response back
+	_, err = syscall.Write(sock, res)
+	if err != nil {
+		panic(err)
+	}
+	syscall.Close(sock)
+}
+
+func worker(id int, socks <-chan int, cache map[string][]byte) {
 	// open conn to backend
 	backendSock, err := NewConnectedSocket(9000, [4]byte{127, 0, 0, 1})
 	if err != nil {
 		panic(err)
 	}
 
-	// listen on localhost 8080
+	fmt.Printf("Worker %v ready\n", id)
+	fmt.Printf("Worker %v backendSock: %v\n", id, backendSock)
+
+	for s := range socks {
+		fmt.Printf("Worker %v starting request\n", id)
+		fmt.Printf("Worker %v backendSock: %v\n", id, backendSock)
+		HandleRequest(s, backendSock, cache)
+		fmt.Printf("Worker %v finished request\n", id)
+	}
+}
+
+func main() {
+	fmt.Println("Proxy starting")
+	// TODO: make this a response obj + marshal
+	cache := make(map[string][]byte)
+
+	// listen on localhost 8000
 	sock, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
 	if err != nil {
 		panic(err)
 	}
-	err = syscall.Bind(sock, &syscall.SockaddrInet4{Port: 8080, Addr: [4]byte{127, 0, 0, 1}})
+	err = syscall.Bind(sock, &syscall.SockaddrInet4{Port: 8000, Addr: [4]byte{127, 0, 0, 1}})
 	if err != nil {
 		panic(err)
 	}
-	err = syscall.Listen(sock, 1)
+	err = syscall.Listen(sock, 64)
 	if err != nil {
 		panic(err)
 	}
 	defer syscall.Close(sock)
 
-	buf := make([]byte, 2048)
+	// idk how much to do here
+	jobs := make(chan int, 64)
+	for w := 1; w <= 1; w++ {
+		go worker(w, jobs, cache)
+	}
+
 	for {
-		// accept conn + read incoming req
+		// accept conn + send sock to workers
 		nfd, _, err := syscall.Accept(sock)
 		if err != nil {
 			panic(err)
 		}
-		n, err := syscall.Read(nfd, buf)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("-> got client request")
-		req, err := ReadRequest(buf[:n])
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("%+v\n", req)
-
-		// proxy request along or get cached result
-		var res []byte
-		var ok bool
-		if strings.HasPrefix(req.Target, prefix) {
-			res, ok = cache[req.Target]
-			if ok {
-				fmt.Printf("Returning cached result for %s\n", req.Target)
-			} else {
-				res, err = ProxyRequest(backendSock, buf[:n])
-				if err != nil {
-					panic(err)
-				}
-				cache[req.Target] = res
-				fmt.Printf("Cached result for %s\n", req.Target)
-			}
-		} else {
-			res, err = ProxyRequest(backendSock, buf[:n])
-			if err != nil {
-				panic(err)
-			}
-		}
-
-		resStruct, err := ReadResponse(res)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("%+v\n", resStruct)
-		fmt.Println("read server response <-")
-
-		// forward response back
-		_, err = syscall.Write(nfd, res)
-		if err != nil {
-			panic(err)
-		}
-		syscall.Close(nfd)
-		fmt.Println("<- response sent to client")
+		// fmt.Printf("New conn sock: %v\n", nfd)
+		jobs <- nfd
 	}
 }
